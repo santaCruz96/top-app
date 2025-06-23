0b09c4f8c201ba691f69add572e1cfb9be99e174
Vyacheslav Ardalin <slavkaardalin@gmail.com>
2025-06-23 21:00:24 +0300

    Инициализация

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            semi: ["error", "always"],
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-empty-interface": "warn",
+        },
+    },
+    eslintConfigPrettier,
 ];
 
 export default eslintConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import React, { JSX } from "react";
+import Head from "next/head";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>MyTop - наш лучший топ</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
